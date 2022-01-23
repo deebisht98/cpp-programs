cd23c7c3de8f9f6b66633773c03d3e9659217d28
Dheeraj bisht <48381903+deebisht98@users.noreply.github.com>
2022-01-23 17:03:27 +0530

    Update rotating array.cpp

--- rotating array.cpp
+++ rotating array.cpp
@@ -4,6 +4,10 @@ void rotate(vector<int>& nums, int k) {
         reverse(nums, 0, k-1);
         reverse(nums, k, nums.size()-1);
     }
+// 1 2 3 4 5 6 7
+// 7 6 5 4 3 2 1 reverse all
+// 5 6 7 4 3 2 1 reverse first k
+// 5 6 7 1 2 3 4 reverse rest
     void reverse(vector<int>& nums, int start, int end) {
         while(start < end){
             int temp  = nums[start];
