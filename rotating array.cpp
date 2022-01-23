a2eb35bc4c09b516d37622dc10ac7c089e95e724
Dheeraj bisht <48381903+deebisht98@users.noreply.github.com>
2022-01-23 17:01:40 +0530

    Update rotating array.cpp

--- rotating array.cpp
+++ rotating array.cpp
@@ -1,13 +1,15 @@
-vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(abs(nums[i]) > abs(nums[j])){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-            }
-            nums[i] = nums[i]*nums[i];
+void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums, 0 , nums.size()-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.size()-1);
+    }
+    void reverse(vector<int>& nums, int start, int end) {
+        while(start < end){
+            int temp  = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-        return nums;
     }
